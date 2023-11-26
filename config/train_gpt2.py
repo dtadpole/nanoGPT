caaf9a0a974e0dc3b69447915e7aa2e6d5d56804
DTadpole <gsk390@gmail.com>
2023-11-26 02:27:49 +0000

    twick

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -8,7 +8,7 @@ wandb_run_name='gpt2-124M'
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
-batch_size = 12
+batch_size = 16
 block_size = 1024
 gradient_accumulation_steps = 5 * 8
 
@@ -17,8 +17,8 @@ max_iters = 600000
 lr_decay_iters = 600000
 
 # eval stuff
-eval_interval = 1000
-eval_iters = 200
+eval_interval = 200
+eval_iters = 1000
 log_interval = 10
 
 # weight decay
